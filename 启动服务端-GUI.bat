f5f0c72c088ba8fcbfbe72c29d5634435a8421dd
absurd <www_1350@163.com>
2026-05-02 06:53:10 +0800

    fix bat

--- 启动服务端-GUI.bat
+++ 启动服务端-GUI.bat
@@ -1,6 +1,6 @@
 @echo off
 @title MapleStory_079
-set path=%PATH%;JAVA_HOME%\bin;%SystemRoot%\system32;%SystemRoot%
+set path=%PATH%;%JAVA_HOME%\bin;%SystemRoot%\system32;%SystemRoot%
 set JRE_HOME=%JAVA_HOME%\jre
 set CLASSPATH=%CLASSPATH%;./*;./lib/*
 java -server -Dwzpath=wz gui.GUIApplication
